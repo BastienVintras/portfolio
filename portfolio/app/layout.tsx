0b260cfb12fa66354dfad0e7a7505af4a6c4e779
BastienVintras <bastvint@gmail.com>
2024-05-14 12:57:33 +0200

    correction vercel

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -5,7 +5,6 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
 import { cn } from "@/lib/utils";
-import { Head } from "next/document";
 
 
 const AnekTelugu = Anek_Telugu({ 
