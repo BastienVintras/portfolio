b77ca209cb35968c1f18fde67eb17fb29485cb59
BastienVintras <bastvint@gmail.com>
2024-05-25 10:37:08 +0200

    integration ga

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -6,33 +6,53 @@ import { GeistMono } from "geist/font/mono";
 
 import { cn } from "@/lib/utils";
 
-
 const AnekTelugu = Anek_Telugu({ 
   subsets: ["latin"],
-  variable : "--font-caption",
+  variable: "--font-caption",
 });
 
 export const metadata: Metadata = {
-  title: "Vintras Bastien . Full Stack Developer",
-  description: "Apprentice Full Stack Developer",
+  title: "Vintras Bastien . Développeur Full Stack",
+  description: "Apprenti Développeur Full Stack",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="fr" className="h-full">
       <head>
-      <link rel="icon" href="/favicon.ico" /> 
-
+        {/* <!-- Google Tag Manager --> */}
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K4GXNXXK');`}
+        </script>
+        {/* <!-- Fin Google Tag Manager --> */}
+        <link rel="icon" href="/favicon.ico" />
       </head>
-      <body className={cn
-        (GeistSans.variable,
-         GeistMono.variable, 
-        AnekTelugu,
-        "font-sans h-full bg-background text-foreground")}>
+      <body
+        className={cn(
+          GeistSans.variable,
+          GeistMono.variable,
+          AnekTelugu.variable,
+          "font-sans h-full bg-background text-foreground"
+        )}
+      >
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K4GXNXXK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- Fin Google Tag Manager (noscript) --> */}
         {children}
       </body>
     </html>
